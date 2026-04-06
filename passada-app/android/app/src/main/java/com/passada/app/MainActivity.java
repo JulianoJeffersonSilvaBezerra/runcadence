35e1bec76a34d036dbd12d23ef203f1ece558eb6
Juliano Jefferson <Juliano.jeff.sb@gmail.com>
2026-04-06 15:58:31 -0300

    nome do app

--- passada-app/android/app/src/main/java/com/passada/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.passada.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- runner-evolution-dashboard/README.md
+++
@@ -1,3 +0,0 @@
-# Runner Evolution Dashboard
-
-Projeto para acompanhar a evolução de corredores com gráficos e métricas.
